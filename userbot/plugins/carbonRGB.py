cec75cf0bf6b703eef418ca3ab9eb20318101ae2
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-12 22:58:03 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -77,7 +77,7 @@ async def carbon_api(e):
    chrome_options.add_argument('--disable-gpu')
    prefs = {'download.default_directory' : './'}
    chrome_options.add_experimental_option('prefs', prefs)
-   await e.edit("⬛⬛⬜⬜⬜")
+   await e.edit("🟪🟦⬜⬜⬜")
 
    driver = webdriver.Chrome(executable_path=Config.CHROME_DRIVER, options=chrome_options)
    driver.get(url)
@@ -90,7 +90,7 @@ async def carbon_api(e):
    sleep(5) # this might take a bit.
    #driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
    #sleep(5)
-   await e.edit("⬛⬛⬛⬜⬜")
+   await e.edit("🟪🟦🟪⬜⬜")
    #driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
    #sleep(5) #Waiting for downloading
 
