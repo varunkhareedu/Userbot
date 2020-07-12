47a4b5559b82b5f6c8bf8d42db65877015421107
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-12 18:09:56 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -10,7 +10,7 @@ from userbot.utils import admin_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-  WARN_PIC = "https://media.tenor.com/images/6870fd2f3f7be6bc6f08083a899c4889/tenor.gif"
+  WARN_PIC = "https://telegra.ph/file/00c3ce38d875988da4c89.jpg"
 else:
   WARN_PIC = PMPERMIT_PIC
 
