124820206949173c6435705deb6f86dcc356470e
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-13 20:43:45 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,6 +8,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/72b8334d6855f6a9c9839.jpg"
 pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
+pm_caption += "**📥Moives Group📥** : `@cinema_lokamm`\n\n\n"
 pm_caption += "**SYSTEM STATUS**\n\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n\n"
