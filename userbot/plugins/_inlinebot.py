65ac055fc3e287cdcf458d881e9f11ce3137bd3c
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-13 17:03:06 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -111,7 +111,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("↩️", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("↪️", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("📤", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("📥", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
