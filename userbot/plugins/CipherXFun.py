af17a986b58ba4bc7d07a5f11dbc0be1e6820124
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-16 21:13:58 +0530

    Delete CipherXFun.py

--- userbot/plugins/CipherXFun.py
+++
@@ -1,9 +0,0 @@
-# Original written by @Hackintush
-"""Available Commands:
-.gam"""
-
-@borg.on(admin_cmd(pattern="gam"))
-async def _(event):
-    if event.fwd_from:
-        return
-    await event.edit("")
