1241531a5e759ac89bc4896139721fbc93040ea7
AmshenShanu <65121810+AmshenShanu07@users.noreply.github.com>
2020-07-22 15:37:42 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -22,7 +22,7 @@ USER_BOT_NO_WARN = ("`Hello My Friend ! This is` **L.I.N.D.A**\n"
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-  WARN_PIC = "https://telegra.ph/file/d81a2b4142c4707e3e9ef.png"
+  WARN_PIC = "https://telegra.ph/file/c16fc7186e362aef3482f.jpg"
 else:
   WARN_PIC = PMPERMIT_PIC
 
