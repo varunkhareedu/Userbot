29fff41127235cbf78b208889e255e881dbd13ea
AmshenShanu <65121810+AmshenShanu07@users.noreply.github.com>
2020-08-25 11:16:55 +0530

    Fix Some bugs

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,21 +7,20 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/72b8334d6855f6a9c9839.jpg"
-pm_caption = "`💠FRIDAY IS💠:` **ONLINE**\n\n"
-pm_caption += "**📥Moives Group📥** : `@cinema_lokamm`\n\n\n"
-pm_caption += "**📥My Group📥** : [🍿Join Movies Group🍿](https://t.me/joinchat/Oq1jlViv1uS2AkOG9MKChw)\n\n"
+pm_caption = "`💠LINDA IS💠:` **ONLINE**\n\n"
+pm_caption += "**📥Master📥** : `@Green_hat_07`\n\n\n"
 
 pm_caption += "**🔖SYSTEM STATUS🔖**\n\n"
 pm_caption += "`⚖️TELETHON VERSION⚖️:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 pm_caption += "`🎢DATABASE STATUS🎢:` **Functional**\n\n"
 pm_caption += "**🧮Current Branch🧮** : `master`\n\n"
-pm_caption += "**📬Friday OS📬** : `3.14`\n\n"
-pm_caption += "**💡Current Sat💡** : `StarkGangSat-2.25`\n\n"
+pm_caption += "**📬Linda OS📬** : `3.14`\n\n"
+pm_caption += "**💡Current Sat💡** : `AmshenShanu07at-2.25`\n\n"
 pm_caption += f"*💜*My Boss💜** : {DEFAULTUSER} \n\n"
 pm_caption += "**💥Heroku Database💥** : `AWS - Working Properly`\n\n"
-pm_caption += "**🚫License🚫** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-pm_caption += "⛔️Copyright : By⛔️ [StarkGang@Github](GitHub.com/StarkGang)\n\n\n\n"
-pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "**🚫License🚫** : [MIT Licence](github.com/AmshenShanu07/Userbot/blob/master/LICENSE)\n\n"
+pm_caption += "⛔️Copyright : By⛔️ [StarkGang@Github](GitHub.com/AmshenShanu07)\n\n\n\n"
+pm_caption += " [Deploy LindaUserbot](https://telegra.ph/)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
